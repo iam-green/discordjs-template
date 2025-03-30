2fb16d2f8ed3e635a520f5ede72d1ef13e60f94d
iam-green <yundohyun050121@gmail.com>
2025-03-30 22:06:12 +0900

    feat: add `bot` configuration for discord bot setup

---
+++ src/config/bot.ts
@@ -0,0 +1,23 @@
+import { ClientOptions, GatewayIntentBits, Locale, Options } from 'discord.js';
+
+export const BotConfig = {
+  NAME: 'Template',
+  COMMAND_PREFIX: ['/', '?', '!', '?!'],
+  DEFAULT_LANGUAGE: 'en-US' as Locale,
+  CLIENT_OPTION: {
+    intents: [
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildVoiceStates,
+    ],
+    sweepers: {
+      ...Options.DefaultSweeperSettings,
+      messages: { interval: 3600, lifetime: 1800 },
+      users: {
+        interval: 3600,
+        filter: () => (user) => user.bot && user.id != user.client.user.id,
+      },
+    },
+  } as ClientOptions,
+};
