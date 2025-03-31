ecf968db3743243fbf56d9d5cdb80d12cd90c402
iam-green <yundohyun050121@gmail.com>
2025-04-01 00:53:24 +0900

    feat: improve command and option localization handling

--- src/structure/language.ts
+++ src/structure/language.ts
@@ -1,7 +1,13 @@
 import { sep } from '@/common';
 import { BotConfig } from '@/config';
-import { Locale } from 'discord.js';
+import {
+  ApplicationCommandType,
+  Locale,
+  SharedNameAndDescription,
+  SlashCommandStringOption,
+} from 'discord.js';
 import { glob } from 'glob';
+import { ExtendedApplicationCommnadType } from './application_command';
 
 export type LanguageData = typeof import('../languages/en-US.json');
 
@@ -40,5 +46,103 @@ export class Language {
     return result.replace(/{(\d+)}/g, (match, number) => {
       return typeof format[number] != 'undefined' ? format[number] : match;
     });
+  }
+
+  static command(
+    name: string,
+  ): Pick<
+    ExtendedApplicationCommnadType<ApplicationCommandType.ChatInput, boolean>,
+    'name' | 'description' | 'localization'
+  > {
+    return {
+      name: Language.get(
+        BotConfig.DEFAULT_LANGUAGE,
+        `Command_${name}_Name` as keyof LanguageData,
+      ),
+      description: Language.get(
+        BotConfig.DEFAULT_LANGUAGE,
+        `Command_${name}_Description` as keyof LanguageData,
+      ),
+      localization: {
+        name: Language.locales()
+          .map((v) => ({
+            [v]: Language.get(v, `Command_${name}_Name` as keyof LanguageData),
+          }))
+          .reduce((a, b) => ({ ...a, ...b })),
+        description: Language.locales()
+          .map((v) => ({
+            [v]: Language.get(
+              v,
+              `Command_${name}_Description` as keyof LanguageData,
+            ),
+          }))
+          .reduce((a, b) => ({ ...a, ...b })),
+      },
+    };
+  }
+
+  static commandOption<T extends SharedNameAndDescription>(
+    command_name: string,
+    command_option: string,
+    option: T,
+  ): T {
+    return option
+      .setName(
+        Language.get(
+          BotConfig.DEFAULT_LANGUAGE,
+          `Command_${command_name}_Option_${command_option}_Name` as keyof LanguageData,
+        ),
+      )
+      .setDescription(
+        Language.get(
+          BotConfig.DEFAULT_LANGUAGE,
+          `Command_${command_name}_Option_${command_option}_Description` as keyof LanguageData,
+        ),
+      )
+      .setNameLocalizations(
+        Language.locales()
+          .map((v) => ({
+            [v]: Language.get(
+              v,
+              `Command_${command_name}_Option_${command_option}_Name` as keyof LanguageData,
+            ),
+          }))
+          .reduce((a, b) => ({ ...a, ...b })),
+      )
+      .setDescriptionLocalizations(
+        Language.locales()
+          .map((v) => ({
+            [v]: Language.get(
+              v,
+              `Command_${command_name}_Option_${command_option}_Description` as keyof LanguageData,
+            ),
+          }))
+          .reduce((a, b) => ({ ...a, ...b })),
+      );
+  }
+
+  static commandOptionChoice(
+    command_name: string,
+    command_option: string,
+    command_choice: string[],
+    option: SlashCommandStringOption,
+  ) {
+    return this.commandOption(command_name, command_option, option).addChoices(
+      command_choice.map((v) => ({
+        name: Language.get(
+          BotConfig.DEFAULT_LANGUAGE,
+          `Command_${command_name}_Option_${command_option}_Choice_${v}` as keyof LanguageData,
+        ),
+        value: v,
+        name_localizations: Language.locales()
+          .map((w) => ({
+            [w]: Language.get(
+              w,
+              `Command_${command_name}_Option_${command_option}_Choice_${v}` as keyof LanguageData,
+            ),
+          }))
+          .reduce((a, b) => ({ ...a, ...b })),
+      })),
+    );
   }
 }
