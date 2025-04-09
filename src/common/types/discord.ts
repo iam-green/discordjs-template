a1dc119ef30ea5267917b9fea89afe5a9cb6df7b
iam-green <yundohyun050121@gmail.com>
2025-04-09 22:55:25 +0900

    refactor: replace `NonNull` with `NonNullable` in `GuildInteraction`

--- src/common/types/discord.ts
+++ src/common/types/discord.ts
@@ -1,15 +1,14 @@
 import { BaseInteraction } from 'discord.js';
-import { NonNull } from './util';
 
 export type GuildInteraction<
   T extends BaseInteraction,
   InGuild extends boolean,
 > = InGuild extends true
   ? T & {
-      guild: NonNull<T['guild']>;
-      guildId: NonNull<T['guildId']>;
-      guildLocale: NonNull<T['guildLocale']>;
-      member: NonNull<T['member']>;
-      memberPermissions: NonNull<T['memberPermissions']>;
+      guild: NonNullable<T['guild']>;
+      guildId: NonNullable<T['guildId']>;
+      guildLocale: NonNullable<T['guildLocale']>;
+      member: NonNullable<T['member']>;
+      memberPermissions: NonNullable<T['memberPermissions']>;
     }
   : T;
--- src/common/types/util.ts
+++ src/common/types/util.ts
@@ -1,3 +1,1 @@
 export type ValueOrArray<T> = T | T[];
-
-export type NonNull<T> = Exclude<T, null | undefined>;
